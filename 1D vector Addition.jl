7f5c3f5bb6d065e32eab327f13789657e9536030
jaideep sai narayan <jaideepsainarayan@gmail.com>
2020-05-30 11:42:41 +0530

    Create 1D vector Addition.jl

---
+++ 1D vector Addition.jl
@@ -0,0 +1,20 @@
+using CUDAdrv, CUDAnative,CuArrays
+function kernel_vadd(a, b, c)
+    i = threadIdx().x
+    c[i] = a[i] + b[i]
+    return
+end
+
+# generate some data
+len = 1024
+a = [i for i=1:len]
+b = [j for j=len:-1:1]
+
+# allocate & upload to the GPU
+d_a = CuArray(a)
+d_b = CuArray(b)
+d_c = similar(d_a)
+
+# execute and fetch results
+@cuda blocks=1 threads=len kernel_vadd(d_a, d_b, d_c)
+print(d_c)
